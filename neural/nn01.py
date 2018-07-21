678d83f028b018bdf75b7c031954e75769114c4e
ami1022 <amiamigo1022@gmail.com>
2018-07-21 18:07:44 +0900

    add new file

---
+++ neural/nn01.py
@@ -0,0 +1,16 @@
+class neuralNetwork:
+
+	#ニューラルネットワークの初期化
+	def __init__(self,inputnodes,hiddennodes,outputnodes,learningrate):
+		self.inodes=inputnodes
+		self.hnodes=hiddennodes
+		self.onodes=outputnodes
+
+		self.lr=learningrate
+		pass
+
+	def train():
+		pass
+
+	def query():
+		pass
